3a9fa4e247ce5eef9af7d2769df6a6600925aa20
prakhar19970 <prakharshukla20160@gmail.com>
2020-08-21 19:40:16 +0530

    changes

--- Database/dbconfig.js
+++ Database/dbconfig.js
@@ -1,7 +1,6 @@
-const password = 'Kochikame02@';
+const password = '***********';
 const connectionString = `mongodb+srv://admin:${password}@cluster0.oruwf.mongodb.net/<funds>?retryWrites=true&w=majority`;
 
 dbCollection='mutual-funds2'
-
 
 module.exports={connectionString,dbCollection}
