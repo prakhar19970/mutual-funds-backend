99c6630f2b87f5a2f5c374d232710c47afb4d3f1
prakhar19970 <prakharshukla20160@gmail.com>
2020-08-26 14:19:55 +0530

    changes in structure

--- controller/mutualFunds.js
+++ controller/mutualFunds.js
@@ -1,54 +1,82 @@
-const { ObjectId } = require("mongodb");
-const { dbCollection }=require("../Database/dbconfig.js")
+
+const express = require('express');
+const app = express();
 
 
- function getAllFunds(db) {
-    return new Promise((resolve, reject) => {
-        db.collection(dbCollection).find().toArray().then(allFunds => {
-            resolve(allFunds)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const funds = require('../controller/mutualFunds.js');
+
+function appStart(db) {
+
+    app.get('/funds', (req, res) => {
+        const fundsData = funds.getAllFunds(db)
+        fundsData.then((allData) => {
+            res.status(200).send(allData)
+        }).catch((err) => {
+            res.sendStatus(404)
         })
-    });
+    })
+
+    app.get('/funds/:id', (req, res) => {
+        const id = req.params.id
+        const fundData = funds.getFund(db, id)
+        fundData.then((singleData) => {
+            console.log(singleData)
+            if (singleData === null) {
+                res.status(400).send(`fund with id: ${id} does not exists`);
+            }
+            else {
+                res.status(200).send(singleData)
+            }
+        }).catch((err) => {
+            res.sendStatus(404)
+        })
+    })
+
+    app.post('/funds', (req, res) => {
+        const newFields = req.body
+        const fundData = funds.createFund(db, newFields)
+        fundData.then((docStatus) => {
+            console.log(docStatus)
+            res.status(202).send(`new fund created with id ${docStatus.insertedId} Status Code:${res.statusCode}`);
+        }).catch((err) => {
+            res.status(404)
+        })
+    })
+
+    app.delete('/funds/:id', (req, res) => {
+        const id = req.params.id
+        const fundData = funds.deleteFund(db, id)
+        fundData.then((docStatus) => {
+            if (docStatus.deletedCount === 0) {
+                res.status(400).send(`fund with id: ${id} does not exists`);
+            } else {
+                res.status(410).send(`Fund with id: ${id} deleted Status Code:${res.statusCode}`);
+            }
+        }).catch((err) => {
+            res.sendStatus(404)
+        })
+    })
+
+    app.put('/funds/:id', (req, res) => {
+        const id = req.params.id
+        const updateFields = req.body
+        const fundData = funds.updateFund(db, id, updateFields)
+        fundData.then((docStatus) => {
+            if (docStatus.matchedCount === 0) {
+                res.status(400).send(`fund with id: ${id} does not exists`);
+            } else {
+                res.status(202).send(`Fund with id: ${id} updated Status Code:${res.statusCode}`);
+            }
+        }).catch((err) => {
+            res.sendStatus(404)
+        })
+    })
+
+
+    app.listen(process.env.PORT || 3000);
+    console.log("server started");
 }
 
-function getFund(db,id) {
-    return new Promise((resolve, reject) => {
-        query={"_id":ObjectId(id)}
-        db.collection(dbCollection).findOne(query).then(fund => {
-            resolve(fund)
-        })
-    });
-}
-
-function createFund(db,newFund){  
-    return new Promise((resolve, reject) => {
-        db.collection(dbCollection).insertOne(newFund).then(status => {
-            resolve(status)
-        })
-    });
-}
-
-function deleteFund(db,id) {
-    return new Promise((resolve, reject) => {
-        query={"_id":ObjectId(id)}
-        db.collection(dbCollection).deleteOne(query).then(status => {
-            resolve(status)
-        })
-    });
-}
-
-function updateFund(db,id,updateFeilds) {
-    return new Promise((resolve, reject) => {
-        query={"_id":ObjectId(id)}
-        newvalues = { $set: updateFeilds };
-        db.collection(dbCollection).updateOne(query,newvalues).then(status => {
-            resolve(status)
-        })
-    });
-}
-
-
-
-
-
-
-module.exports = { getAllFunds, getFund, createFund, deleteFund ,updateFund}
+module.exports = { appStart };
--- jsonTOmongo.js
+++ jsonTOmongo.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const MongoClient = require('mongodb').MongoClient
-const password = 'Kochikame02@';
+const password = process.env.DB_PASS;
 const connectionString = `mongodb+srv://admin:${password}@cluster0.oruwf.mongodb.net/<funds>?retryWrites=true&w=majority`;
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
