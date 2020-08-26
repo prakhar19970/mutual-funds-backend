f849d953bad512623d1dbfef43dba127e373adda
prakhar19970 <prakharshukla20160@gmail.com>
2020-08-26 16:07:28 +0530

    vaidations added and services updated

--- controller/mutualFunds.js
+++ controller/mutualFunds.js
@@ -10,54 +10,81 @@ const funds = require('../model/fund.js');
 function appStart(db) {
 
     app.get('/funds', (req, res) => {
-        const fundsData = funds.getAllFunds(db)
-        fundsData.then((allData) => {
-            res.status(200).send(allData)
-        }).catch((err) => {
-            res.sendStatus(404)
-        })
+        if (req.query.search) {
+            const id = req.query.search
+            const fundData = funds.getFund(db, id)
+            fundData.then((singleData) => {
+                console.log(singleData)
+                if (singleData === null) {
+                    res.status(400).send(`fund with id: ${id} does not exists`);
+                }
+                else {
+                    res.status(200).send(singleData)
+                }
+            }).catch((err) => {
+                res.sendStatus(404)
+            })
+        }
+        else {
+            const fundsData = funds.getAllFunds(db)
+            fundsData.then((allData) => {
+                res.status(200).send(allData)
+            }).catch((err) => {
+                res.status(404).send('Request Not Found')
+            })
+        }
     })
 
     app.get('/funds/:id', (req, res) => {
         console.log(req.query)
-        // const id = req.params.id
-        // const fundData = funds.getFund(db, id)
-        // fundData.then((singleData) => {
-        //     console.log(singleData)
-        //     if (singleData === null) {
-        //         res.status(400).send(`fund with id: ${id} does not exists`);
-        //     }
-        //     else {
-        //         res.status(200).send(singleData)
-        //     }
-        // }).catch((err) => {
-        //     res.sendStatus(404)
-        // })
+        const id = req.params.id
+        const fundData = funds.getFund(db, id)
+        fundData.then((singleData) => {
+            console.log(singleData)
+            if (singleData === null) {
+                res.status(400).send(`fund with id: ${id} does not exists`);
+            }
+            else {
+                res.status(200).send(singleData)
+            }
+        }).catch((err) => {
+            res.status(404).send("Fund not found")
+        })
     })
 
     app.post('/funds', (req, res) => {
         const newFields = req.body
-        const fundData = funds.createFund(db, newFields)
-        fundData.then((docStatus) => {
-            console.log(docStatus)
-            res.status(202).send(`new fund created with id ${docStatus.insertedId} Status Code:${res.statusCode}`);
-        }).catch((err) => {
-            res.status(404)
-        })
+        if (Object.entries(newFields).length) {
+            const fundData = funds.createFund(db, newFields)
+            fundData.then((docStatus) => {
+                console.log(docStatus)
+                res.status(202).send(`new fund created with id ${docStatus.insertedId} Status Code:${res.statusCode}`);
+            }).catch((err) => {
+                res.status(404)
+            })
+        }
+        else {
+            res.status(400).send(`Invalid Input`);
+        }
+    })
+
+    app.delete('/funds',(req,res)=>{
+        res.status(404).send("Not Found : Invalid Input")
     })
 
     app.delete('/funds/:id', (req, res) => {
         const id = req.params.id
-        const fundData = funds.deleteFund(db, id)
-        fundData.then((docStatus) => {
-            if (docStatus.deletedCount === 0) {
-                res.status(400).send(`fund with id: ${id} does not exists`);
-            } else {
-                res.status(410).send(`Fund with id: ${id} deleted Status Code:${res.statusCode}`);
-            }
-        }).catch((err) => {
-            res.sendStatus(404)
-        })
+        console.log(typeof(id))
+            const fundData = funds.deleteFund(db, id)
+            fundData.then((docStatus) => {
+                if (docStatus.deletedCount === 0) {
+                    res.status(400).send(`fund with id: ${id} does not exists`);
+                } else {
+                    res.status(410).send(`Fund with id: ${id} deleted Status Code:${res.statusCode}`);
+                }
+            }).catch((err) => {
+                res.status(404).send("Not Found : Invalid Input")
+            })
     })
 
     app.put('/funds/:id', (req, res) => {
--- model/fund.js
+++ model/fund.js
@@ -1,8 +1,8 @@
 const { ObjectId } = require("mongodb");
-const { dbCollection }=require("../Database/dbconfig.js")
+const { dbCollection } = require("../Database/dbconfig.js")
 
 
- function getAllFunds(db) {
+function getAllFunds(db) {
     return new Promise((resolve, reject) => {
         db.collection(dbCollection).find().toArray().then(allFunds => {
             resolve(allFunds)
@@ -10,16 +10,16 @@ const { dbCollection }=require("../Database/dbconfig.js")
     });
 }
 
-function getFund(db,id) {
+function getFund(db, id) {
     return new Promise((resolve, reject) => {
-        query={"_id":ObjectId(id)}
+        query = { "_id": ObjectId(id) }
         db.collection(dbCollection).findOne(query).then(fund => {
             resolve(fund)
         })
     });
 }
 
-function createFund(db,newFund){  
+function createFund(db, newFund) {
     return new Promise((resolve, reject) => {
         db.collection(dbCollection).insertOne(newFund).then(status => {
             resolve(status)
@@ -27,23 +27,23 @@ function createFund(db,newFund){
     });
 }
 
-function deleteFund(db,id) {
-    return new Promise((resolve, reject) => {
-        query={"_id":ObjectId(id)}
-        db.collection(dbCollection).deleteOne(query).then(status => {
-            resolve(status)
-        })
-    });
+function deleteFund(db, id) {
+    // return new Promise((resolve, reject) => {
+    //     query = { "_id": ObjectId(id) }
+    //     db.collection(dbCollection).deleteOne(query).then(status => {
+    //         resolve(status)
+    //     })
+    // });
 }
 
-function updateFund(db,id,updateFeilds) {
+function updateFund(db, id, updateFeilds) {
     return new Promise((resolve, reject) => {
-        query={"_id":ObjectId(id)}
+        query = { "_id": ObjectId(id) }
         newvalues = { $set: updateFeilds };
-        db.collection(dbCollection).updateOne(query,newvalues).then(status => {
+        db.collection(dbCollection).updateOne(query, newvalues).then(status => {
             resolve(status)
         })
     });
 }
 
-module.exports = { getAllFunds, getFund, createFund, deleteFund ,updateFund}
+module.exports = { getAllFunds, getFund, createFund, deleteFund, updateFund }
